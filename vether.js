32a6a6279e838310cc59beb4151e2ec84f62eb34
Augustin Berisa <burim.berisa@protonmail.com>
2021-04-17 13:27:21 +0200

    Update contract address and abi

--- vether.js
+++ vether.js
@@ -1,7 +1,7 @@
 const VETHER = require('./DeployedVether.json')
 
 const addr = () => {
-	return '0x31Bb711de2e457066c6281f231fb473FC5c2afd3'
+	return '0x4Ba6dDd7b89ed838FEd25d208D4f644106E34279'
 }
 
 const abi = () => {
